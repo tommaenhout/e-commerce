1daeb94bdabd13cfd2e08dbf86c7100a1bf99baf
tommaenhout <tom.maenhout@gmail.com>
2023-06-12 22:22:12 -0300

    add products and useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+   
+    const getData = async (url) => {
+        try {
+            const response = await axios.get(url)
+            console.log(response.data)
+            setData(response.data)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+     
+    
+
+    return { data, error, loading, getData }
+}
+
+export default useFetch
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,15 @@
 import ItemListContainer from "../components/ItemListContainer";
+import {useEffect} from "react";
+import useFetch from "../hooks/useFetch";
+
 
 const Homepage = () => {
+    const {data,error,getData,loading} = useFetch()
+    useEffect(() => {
+        getData("../../products.json")
+    }, [])
+
+    console.log(data)
     return (
         <div>
             <ItemListContainer greeting="Welcome to the Muay Thai shop"/>
